9b3e308f3379fefa50f2ffa2c32807c7179e2576
Mohamed Farah <mwa65867@gmail.com>
2021-11-25 12:21:48 -0600

    Time: 956 ms (88.20%), Space: 25.1 MB (83.31%) - LeetHub

--- best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,40 +1,14 @@
-# Previous solution that was O(n) to O(n^2)
-
-# class Solution:
-#     def maxProfit(self, prices: List[int]) -> int:
-#         maxProfit = 0
-#         numberOfDays = len(prices)
-
-#         # Was receiving time exceeded messages, so I elminated redundancy using Kadane's algorithm (basically, stop trying to find the same number again, as long as the maximum has already been established)
-#         maximumInTheInterval = max(prices)
-    
-    
-#         for i in range(numberOfDays):
-            
-#             prevMax = maxProfit
-            
-#             if i == numberOfDays-1:
-#                 break
-                            
-#             if prices[i] == maximumInTheInterval:
-#                 maximumInTheInterval = max(prices[i+1:numberOfDays])
-            
-#             maxProfit = max(prevMax, 
-#                             maximumInTheInterval - prices[i]
-#                            )
-
-#         return maxProfit
-
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        minprice = 10**4
+        maxprofit = 0
         
-        # I was overthinking it, I just need one pass through and find the minimum, then the profit relative to the last minimum and the current price, then find their max
+        for i in range(len(prices)):
+            if prices[i] < minprice:
+                minprice = prices[i]
+                
+            elif prices[i] - minprice > maxprofit:
+                maxprofit = prices[i] - minprice
         
-        minimum, maximum, maximumProfit = float("inf"), 0, 0
-        
-        for price in prices:
-            minimum = min(minimum, price)
-            profit = price - minimum
-            maximumProfit = max(profit, maximumProfit)
-
-        return maximumProfit
+        return maxprofit
+            
