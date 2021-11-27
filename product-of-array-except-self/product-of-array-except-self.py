c9f2a4995b0b4a1a099cf6d654e918a4a6ab6d5c
Mohamed Farah <mwa65867@gmail.com>
2021-11-27 17:04:52 -0600

    Time: 2172 ms (5.00%), Space: 37.7 MB (8.58%) - LeetHub

--- product-of-array-except-self/product-of-array-except-self.py
+++ product-of-array-except-self/product-of-array-except-self.py
@@ -1,22 +1,27 @@
 from numpy import prod
 
+# My mistake was that I was doing unnecessary calculations, I would take the product for every element when I only needed a product from before
+
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         
         sol,sol2 = [],[]
         
+        # First pass through
         curProd = 1
         sol.append(curProd)
         for i in range(1,len(nums)):
             curProd = nums[i-1]*curProd
             sol.append(curProd)
         
+        # Second pass through
         curProd = 1
         sol2.insert(0,curProd)
         for i in range(len(nums)-2,-1,-1):
             curProd = nums[i+1]*curProd
             sol2.insert(0,curProd)
         
+        # Product of both passes
         for i in range(len(sol2)):
             sol[i] = int(sol[i]*sol2[i])
             
