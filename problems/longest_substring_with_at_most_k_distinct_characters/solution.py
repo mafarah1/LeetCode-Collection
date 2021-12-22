385538bd0c2fab7cef3f681712b93925ccee1441
Mohamed Farah <mwa65867@gmail.com>
2021-12-22 14:15:28 +0000

    Sync LeetCode submission - Longest Substring with At Most K Distinct Characters (python3)

---
+++ problems/longest_substring_with_at_most_k_distinct_characters/solution.py
@@ -0,0 +1,23 @@
+class Solution:
+    def lengthOfLongestSubstringKDistinct(self, str1: str, k: int) -> int:
+        window_start = 0
+        max_length = 0
+        char_frequency = {}
+
+        # in the following loop we'll try to extend the range [window_start, window_end]
+        for window_end in range(len(str1)):
+            right_char = str1[window_end]
+            if right_char not in char_frequency:
+                char_frequency[right_char] = 0
+            char_frequency[right_char] += 1
+
+            # shrink the sliding window, until we are left with 'k' distinct characters in the char_frequency
+            while len(char_frequency) > k:
+                left_char = str1[window_start]
+                char_frequency[left_char] -= 1
+                if char_frequency[left_char] == 0:
+                    del char_frequency[left_char]
+                window_start += 1  # shrink the window
+            # remember the maximum length so far
+            max_length = max(max_length, window_end-window_start + 1)
+        return max_length
